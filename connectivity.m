24217b55dee3d0b4296db215989421bb5ee53e79
kathleneee <36493045+kathleneee@users.noreply.github.com>
2018-02-20 07:51:30 -0800

    Update connectivity.m

--- connectivity.m
+++ connectivity.m
@@ -7,11 +7,11 @@ zk=z;
 iflag1 = 0; % iflag1 and iflag2 count the number of iterations
 iflag2 = 0; % when a root is within 1.e^(-6) of a fixed point;
 kount = 0; % kount is the total number of iterations.
-while kount < 100 & abs(zk) < 2 & iflag1 <5 &iflag2 <5,
+while kount < 100,
     kount = kount + 1;
     zk = phi(zk); % fixed point iteration
-    if abs(zk) > 100; fprintf("Julia set with complex number %s and starting value of 0 diverges.\n",c); end; % divergence
-    if abs(zk) < (10^-6); end;
+    if abs(zk) > 100, fprintf("Julia set with complex number %s and starting value of 0 diverges.\n",c), end; % divergence
+    if abs(zk) < (10^-6), end;
 end;
          
   
