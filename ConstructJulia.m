48e25c7fbb35038da47dfb15ebdab98d266793dc
Adam Kagel <kagelosaurus@gmail.com>
2018-02-21 01:44:54 -0800

    editted part 3

--- ConstructJulia.m
+++ ConstructJulia.m
@@ -1,21 +1,28 @@
-function ConstructJulia(c,num_points)
-pick = round(rand);
-if pick
-    julia_set = 0.5*(1 + sqrt(1-4*c));
-else
-    julia_set = 0.5*(1 - sqrt(1-4*c));
+function ConstructJulia(c,num_points, iterations)
+
+% final julia set
+julia_set = zeros(num_points,iterations);
+
+% num_points random starting points
+z = (rand(num_points,1)*2-1) + (rand(num_points,1)*2-1)*1i;
+
+% Iterate 15 times to remove non boundary points
+for k = 1:15
+   z = sqrt(z - c);
 end
-for k = 1:num_points
-    pick = round(rand);
-    if pick
-        next = sqrt(julia_set(end) - c); 
-    else
-        next = -sqrt(julia_set(end) - c);
-    end
-    julia_set = [julia_set; next];
+
+for k = 1:iterations
+    z = sqrt(z - c);
+    idx = rand(num_points,1)>0.5;
+    z(idx) = -z(idx);
+    julia_set(:,k) = z;
 end
 
 figure
-plot(julia_set, '.')
+plot(julia_set, 'k.')
+ax = gca;
+ax.Visible = 'off';
+
+print(gcf,'-dpng');
 
 end
